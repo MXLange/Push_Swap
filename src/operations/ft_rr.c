35c5f44893bbff83c2871185fc29e403ddee66a7
MXLange <114676318+MXLange@users.noreply.github.com>
2022-12-13 12:26:23 +0000

    up

--- src/operations/ft_rr.c
+++ src/operations/ft_rr.c
@@ -24,5 +24,6 @@ void	ft_rr(t_stack **a, t_stack **b)
 	temp->next = (*b);
 	(*b) = (*b)->next;
 	temp->next->next = NULL;
-	ft_putstr_fd("rr\n", 1);
+	//ft_putstr_fd("rr\n", 1);
+	ft_printf("rr\n");
 }
