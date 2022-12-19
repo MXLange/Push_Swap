4c19d539d25c606944db827e26853a3b152d7f11
Murillo Sprengel Lange <lange_sp@hotmail.com>
2022-12-19 21:40:44 +0000

    b change

--- src/algorithm/ft_push_solve_b.c
+++ src/algorithm/ft_push_solve_b.c
@@ -6,7 +6,7 @@
 /*   By: msprenge <msprenge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 18:28:57 by msprenge          #+#    #+#             */
-/*   Updated: 2022/12/12 18:57:27 by msprenge         ###   ########.fr       */
+/*   Updated: 2022/12/19 21:40:01 by msprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 size_t	ft_push_solve_b(t_stack **a, t_stack **b, int column_mid_point)
 {
 	ft_pb(&(*a), &(*b));
-	if ((*b)->index > (size_t)column_mid_point)
+	if ((*b)->index > (size_t)column_mid_point && ft_stack_size((*b)) > 1)
 	{
 		if ((*a)->index > (*a)->next->index)
 			ft_rr(&(*a), &(*b));
